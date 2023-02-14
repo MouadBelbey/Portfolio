c4807e0f8d68e9fc94da67cabf544423dd14bfcd
MouadB <mouadbelbey@yahoo.fr>
2023-02-14 12:11:58 -0500

    Adding new CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import CV from '../../assets/Mouad_Belbey_curriculum_Vitae.pdf'
+import CV from '../../assets/Mouad_Belbey_curriculum_Vitae_Can.pdf'
 import Certificat from '../../assets/Certificat.pdf'
 
 function CTA() {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href={CV} download className='btn'>CV</a>
+        <a href="#contact" className='btn btn-primary'>Me joindre</a>
         <a href={Certificat} download className='btn'>Certificat</a>
     </div>
   )
